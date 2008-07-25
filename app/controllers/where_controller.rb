3c24fb9af7b6982ef3b09a789a755696dce7c783
Brad Gilreath <bgilreath@ScrapBook.local>
2008-07-25 11:18:28 -0400

    Added more Where support

--- app/controllers/where_controller.rb
+++ app/controllers/where_controller.rb
@@ -1,5 +1,26 @@
-class WhereController < ApplicationController
-  def index
+class WhereController < ApplicationController  
+  def show
+    
+    #validate query strings
+    #Need to make sure Emulators can't win
+    #visibility based on carrier or phone type as well
+    
+     @prize = Prize.find(params[:id])
+
+      respond_to do |format|
+          #format.xml  { render :xml => @prize }    
+        format.jin  { render :jin => @prize }
+          #format.bwg  { render :xml => @prize }
+      end
+  
     
   end
+  
+  def debugwhere
+  
+  end
+  
+  def chk4prize
+  end
+  
 end
