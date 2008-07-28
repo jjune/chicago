5e332160be2dc40d82ec56717e87697a9c9df5b9
Brad Gilreath <bgilreath@ScrapBook.local>
2008-07-27 21:37:17 -0400

    Where controller modifications

--- app/controllers/where_controller.rb
+++ app/controllers/where_controller.rb
@@ -21,6 +21,27 @@ class WhereController < ApplicationController
   end
   
   def chk4prize
+  
+  #Do we have this person's device ID on file?
+  #Is this valid query
+  
+    current_marker = Point.from_lon_lat(params[:lng],params[:lat],4326)
+    prizes = Prize.find_all_by_prizearea(current_marker)
+  
+    prizes.each do |prize|
+  		  #@polygon = GPolygon.from_georuby(prize.prizearea,'#0000FF','5','1','#0000FF','0.2')
+  		  render :update do |page|
+	   		  notice=prizes.length
+	
+			    page.hide 'notice'
+			
+			    page.replace_html 'notice', notice
+			    page.visual_effect :appear, 'notice', :duration => 0.5
+			
+			    page << @map.clear_overlays
+			    page << @map.add_overlay(@polygon)	
+  		  end
+      end
   end
   
 end
