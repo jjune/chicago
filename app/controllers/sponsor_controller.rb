3bf55001a1010bcffa2c4f39743c5a05537beec7
Brad Gilreath <bgilreath@ScrapBook.local>
2008-08-02 19:48:37 -0400

    Tons o style changes

--- app/controllers/sponsor_controller.rb
+++ app/controllers/sponsor_controller.rb
@@ -1,4 +1,22 @@
 class SponsorController < ApplicationController
+ 
+   before_filter :login_required
   def index
+     @header_above_partial_path = "global/hdr_above"
+      @header_partial_path = "prizes/hdr_prizelab"
+      @header_below_partial_path = "global/hdr_below"
+      @hdr_below_title = "Dashboard"
+
+      @footer_above_partial_path = "global/ftr_above"
+      #@footer_partial_path = "prizes/hdr_prizelab"
+      @footer_below_partial_path = "global/ftr_below"
+      #@ftr_below_title = "Dashboard"
+
+      @prizes = Prize.find(:all)
+
+      respond_to do |format|
+        format.html # index.html.erb
+        format.xml  { render :xml => @prizes }
+      end
   end
 end
