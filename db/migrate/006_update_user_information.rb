64405dffbb3bf04eb5624b420e1b251060f232e3
Joseph June <Joseph@annie-junes-macbook-pro-15.local>
2008-08-01 23:31:42 -0400

    Created 006 update_user_information

---
+++ db/migrate/006_update_user_information.rb
@@ -0,0 +1,16 @@
+class UpdateUserInformation < ActiveRecord::Migration
+  def self.up
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address1, :string
+    add_column :users, :address2, :string
+    add_column :users, :city,     :string
+    add_column :users, :state,    :string
+    add_column :users, :zipcode,  :string
+    add_column :users, :phone_number, :string
+    remove_column :users, :device_type, :string
+  end
+
+  def self.down
+  end
+end
