7fe21151259bd596d472d7f33bc012ac7afbabb4
Brad Gilreath <bgilreath@68-240-173-154.area5.spcsdns.net>
2008-09-17 14:09:26 -0400

    Adjust to eliminate paginate

--- app/controllers/prizes_controller.rb
+++ app/controllers/prizes_controller.rb
@@ -13,10 +13,11 @@ class PrizesController < ApplicationController
     @footer_below_partial_path = "global/ftr_below"
     #@ftr_below_title = "Dashboard"
     
-    #@prizes = Prize.find(:all)
+    
     
     #This is the problem because I used old technique for pagination
-    @prize_pages, @prizes = paginate :prizes, :per_page => 3
+    #@prize_pages, @prizes = paginate :prizes, :per_page => 3
+    @prizes = Prize.find(:all)
 
     respond_to do |format|
       format.html # index.html.erb
