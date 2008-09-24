e38eda029c65c9b6d61aa7c59df64b8aa520dfdc
Joseph June <Joseph@joseph-junes-mac-pro.local>
2008-09-24 16:34:05 -0400

    Adjusted default homepage to be the login page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :users
   map.resources :prizes
+  map.connect '/', :controller => 'account', :action => 'login'
   map.connect 'register/sponsor', :controller => 'account', :action => 'sponsor_registration'
   map.connect 'register/player', :controller => 'account', :action => 'player_registration'
   map.connect 'register', :controller => 'account', :action => 'player_registration'
   map.connect 'register/contact', :controller => 'account', :action => 'register_contact'
   map.connect 'login', :controller => 'account', :action => 'login'
-  map.connect 'register/device', :controller => 'account', :action => 'register_device'
+  map.root :controller => 'account', :action => 'register_device'
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
   #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
@@ -31,9 +30,6 @@ ActionController::Routing::Routes.draw do |map|
   #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
   #     admin.resources :products
   #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
 
   # See how all your routes lay out with "rake routes"
 
