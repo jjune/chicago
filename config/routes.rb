a25d1e27f4cb070984ef9e74ce054661f1ffcbca
Joseph June <Joseph@joseph-junes-mac-pro.local>
2008-10-17 19:01:32 -0400

    Added additional routes for better flow

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :session
   map.resources :prizes
   map.connect '/', :controller => 'sessions', :action => 'new'
+  map.connect 'sponsor/dashboard', :controller => 'prizes', :action => 'index'
   map.connect 'register/sponsor', :controller => 'account', :action => 'sponsor_registration'
   map.connect 'register/player', :controller => 'account', :action => 'player_registration'
   map.connect 'register', :controller => 'account', :action => 'player_registration'
@@ -12,32 +13,6 @@ ActionController::Routing::Routes.draw do |map|
   map.connect '/activate/:activation_code', :controller => 'users', :action => 'activate'
   map.root :controller => 'account', :action => 'register_device'
   
-  
-  # The priority is based upon order of creation: first created -> highest priority.
-
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
