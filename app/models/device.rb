673771913d9bbed5b9364d4db3e2eed9622ab548
Keith Litwin <keith74@gmail.com>
2008-10-28 09:38:55 -0500

    dry'd up the short code generation code

--- app/models/device.rb
+++ app/models/device.rb
@@ -44,7 +44,7 @@ class Device < ActiveRecord::Base
 	end
 	
 	def self.generate_short_code
-		a=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"]
+		a=("A".."Z").to_a + ("0".."9").to_a
 		short_code=""
 		10.times do 
 			short_code<<a[rand(a.length-1)]
