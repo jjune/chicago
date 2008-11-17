7d2f82c2a84f2a31e6a62d181292a813892b30ec
Brad Gilreath <bgilreath@ScrapBook.local>
2008-11-16 21:45:20 -0500

    adjusted paypal ipn controller

--- app/controllers/hiccup_controller.rb
+++ app/controllers/hiccup_controller.rb
@@ -3,6 +3,21 @@ class HiccupController < ApplicationController
 #This is the PayPal Instant Payment Notification
 #It is named this way to prevent people from hacking
 
+def paypal_ipn
+  @raw = request.raw_post
+  logger.debug "Raw Post = #{@raw}"
+  logger.debug "params = #{params}"
+  @ipn = Ipn.new
+  @ipn.payer_email = params[:payer_email]
+  @ipn.payment_gross = params[:payment_gross]
+  @ipn.payment_date = params[:payment_date]
+  @ipn.raw_post = @raw
+  @ipn.verified = ipnConfirm
+  @ipn.save
+
+end
+
+
 def ipnconfirm
 
 uri = URI.parse("http://www.sandbox.paypal.com/cgi-bin/webscr")
