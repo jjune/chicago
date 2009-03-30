db461a8ab8f0678cb6086922b6f26cb57f5c108b
Keith Litwin <keith74@gmail.com>
2009-03-29 21:11:14 -0400

    mobile

---
+++ lib/devices/win_ce.rb
@@ -0,0 +1,63 @@
+#lng, lat, and georuby_point should be defined in every device module at a minimum
+module Devices
+	module WinCe
+		
+		def lng
+		    if @current_request.parameters[:lng].nil?
+		      "-84.49008"
+		    else
+		      @current_request.parameters[:lng]
+		    end
+		end
+		
+		def lat
+			if @current_request.parameters[:lat].nil?
+				"33.84275"
+	    	else
+	      		@current_request.parameters[:lat]
+	    	end
+		end
+		
+		def georuby_point
+			Point.from_lon_lat(lng,lat,4326)
+		end
+	
+		def device_type
+			if @current_request.parameters[:x-wap-profile].nil?
+				"unknown"
+			else
+				@current_request.parameters[:x-wap-profile]
+			end
+		end
+		
+		def carrier
+			if @current_request.parameters[:carrier].nil?
+				"Emulator"
+			else
+				@current_request.parameters[:carrier]
+			end
+		end
+		
+		def screenwidth
+			if @current_request.parameters[:UA-pixels].nil?
+				"240"
+			else
+				pixels = @current_request.parameters[:UA-pixels].split("x")
+				pixels[0]
+			end
+		end
+		
+		def textsize
+			if screenwidth=="240"
+	        	"small"
+	      else
+	        	"medium"
+	      end
+		end
+		
+		def user_agent
+			@current_request.user_agent
+		end
+		
+	end
+end
